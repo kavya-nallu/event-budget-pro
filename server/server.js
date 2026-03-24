b6693c7b9fe58cbc1aeff762335c82e333303854
kavyaa <kavyanallu111@gmail.com>
2026-03-24 10:01:39 +0530

    added test route

--- server/server.js
+++ server/server.js
@@ -147,3 +147,6 @@ app.put('/api/events/update-event/:id', async (req, res) => {
 // ====== Start Server ======
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
