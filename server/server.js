5e84818f1e560057ee80090bfbf061c264493771
kavyaa <kavyanallu111@gmail.com>
2026-03-24 10:23:40 +0530

    fix port for Azure

--- server/server.js
+++ server/server.js
@@ -145,7 +145,7 @@ app.put('/api/events/update-event/:id', async (req, res) => {
 });
 
 // ====== Start Server ======
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
